3add4baa04a4a55eeb4b15428d575026a3da5bde
Tamer <79remat@gmail.com>
2023-10-25 10:32:08 -0400

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -22,14 +22,6 @@
 
 	
 	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 60) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
-    });
 
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
